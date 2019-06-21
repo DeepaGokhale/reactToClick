90385fd545a7c1bbed4d67db518e0f40d6dd2fa4
dbhujle <bhujle.Deepa@gmail.com>
2019-06-21 18:50:49 -0500

    the shuffling and stff

--- src/App.js
+++ src/App.js
@@ -14,26 +14,34 @@ class App extends React.Component{
 
 
   shuffleImages = () => {
-    var imgArr = shuffle(this.state.imagesList);
-    console.log(imgArr);
+    let imgArr = shuffle(this.state.imagesList);
+    // console.log(imgArr);
     this.setState({imagesList: imgArr});
   }
 
   handleIncrement = () => {
+    console.log( 1 + 1);
     this.setState({ count: this.state.count + 1 });
   };
 
   //Click handler decides the score and also shuffles the cards on click event
-  clickHandler = clicked => {
+  clickHandler = () => {
     //if true means already clicked
-    if(clicked)
+    if(this.clicked)
       {
         //set the score to 0 and set all the images clicked to false(in other workds load the original images list)
-
+        this.Score = 0;
+        let imgArray = this.state.imagesList;
+        imgArray.forEach = img => img.clicked = false;
+        console.log(imgArray);
+        this.shuffleImages();
       }
       else  // false so increse the score by 1
       {
-        
+        this.setState({clicked: true});
+        // this.setState({clicked: true});
+        this.handleIncrement();
+        this.shuffleImages();
       }
       //regardless images are shuffled
 
@@ -49,12 +57,12 @@ class App extends React.Component{
               id = {image.id}          
               image = {image.image}
               clicked = {image.clicked}
-              // clickHandler = {this.clickHandler}
-              shuffleImages = {this.shuffleImages}
-              handleIncrement = {this.handleIncrement}
-            />
+              clickHandler = {this.clickHandler}
+              // shuffleImages = {this.shuffleImages}
+              // handleIncrement = {this.handleIncrement}
+              />
+            )
           )
-        )
         }
       </Wrapper>
     );
